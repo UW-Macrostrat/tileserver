a32a366d259a5031fd5036f2a0a69c3b44340270
Daven Quinn <code@davenquinn.com>
2024-05-02 12:06:46 -0400

    Created new version of units query

--- macrostrat_tileserver/filterable/__init__.py
+++ macrostrat_tileserver/filterable/__init__.py
@@ -1,20 +1,47 @@
+from pathlib import Path
+
 from buildpg import render
 from fastapi import APIRouter, Request, Response
 from timvt.resources.enums import MimeTypes
 
 router = APIRouter()
 
+__here__ = Path(__file__).parent
+
 
 @router.get("/carto/{z}/{x}/{y}")
 async def get_tile(request: Request, z: int, x: int, y: int):
     """Get a tile from the tileserver."""
     pool = request.app.state.pool
-    q, p = render(
-        "SELECT tile_layers.carto_slim(:x::integer, :y::integer, :z::integer, '{}') AS data;",
-        z=z,
-        x=x,
-        y=y,
-    )
+
+    units_query = __here__ / "queries" / "units.sql"
+
+    q = units_query.read_text()
+    q = q.strip()
+    if q.endswith(";"):
+        q = q[:-1]
+
+    # Replace the envelope with the function call
+    q = q.replace(":envelope", "tile_utils.envelope(:x, :y, :z)")
+
+    # Wrap with MVT creation
+    q1 = f"WITH feature_query AS ({q}) SELECT ST_AsMVT(feature_query, :layer_name) FROM feature_query"
+
+    if z < 3:
+        # Select from carto.tiny table
+        mapsize = "tiny"
+        linesize = ["tiny"]
+    elif z < 6:
+        mapsize = "small"
+        linesize = ["tiny", "small"]
+    elif z < 9:
+        mapsize = "medium"
+        linesize = ["small", "medium"]
+    else:
+        mapsize = "large"
+        linesize = ["medium", "large"]
+
+    q, p = render(q1, z=z, x=x, y=y, layer_name="units", mapsize=mapsize)
     async with pool.acquire() as con:
         data = await con.fetchval(q, *p)
     kwargs = {}
---
+++ macrostrat_tileserver/filterable/queries/units.sql
@@ -0,0 +1,24 @@
+WITH mvt_features AS (
+SELECT
+  map_id,
+  source_id,
+  geom
+FROM
+  carto.polygons
+WHERE scale::text = :mapsize
+  AND ST_Intersects(geom, ST_Transform(:envelope, 4326))
+)
+SELECT
+  z.map_id,
+  z.source_id,
+  l.*, -- legend info
+  tile_layers.tile_geom(z.geom, :envelope) AS geom
+FROM
+  mvt_features z
+  LEFT JOIN maps.map_legend ON z.map_id = map_legend.map_id
+  LEFT JOIN tile_layers.map_legend_info AS l
+    ON l.legend_id = map_legend.legend_id
+  LEFT JOIN maps.sources
+    ON z.source_id = sources.source_id
+WHERE
+  sources.status_code = 'active'
