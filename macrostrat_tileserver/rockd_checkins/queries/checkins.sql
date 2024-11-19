a3a20a6272122cc9fea0d5fc9dfd52db72f4464d
Daven Quinn <code@davenquinn.com>
2024-11-18 22:08:53 -0600

    Improve checkins query

--- macrostrat_tileserver/rockd_checkins/queries/checkins.sql
+++ macrostrat_tileserver/rockd_checkins/queries/checkins.sql
@@ -1,12 +1,21 @@
 WITH tile AS (
   SELECT
     ST_TileEnvelope(:z, :x, :y) AS envelope
-), checkins AS (SELECT person_id,
-                       notes,
-                       rating,
-                       ST_AsMVTGeom(ST_Transform(ST_SetSRID(geom,4326), 3857), tile.envelope)
-                FROM public.checkins,
-                     tile
-                WHERE geom IS NOT NULL
-                  AND ST_Intersects(ST_SetSRID(geom, 4326), ST_Transform(tile.envelope, 4326)))
- SELECT ST_AsMVT(checkins, 'default') FROM checkins
+), checkins AS (
+  SELECT
+    checkin_id id,
+    person_id person,
+    notes,
+    rating,
+    date_part('year', coalesce(updated, created)) AS year,
+    ST_AsMVTGeom(ST_Transform(ST_SetSRID(geom,4326), 3857), tile.envelope)
+  FROM public.checkins,
+       tile
+  WHERE geom IS NOT NULL
+    AND status = 1 -- Only public checkins that haven't been flagged
+    AND ST_Intersects(
+    ST_SetSRID(geom, 4326),
+    ST_Transform(tile.envelope, 4326)
+    )
+)
+SELECT ST_AsMVT(checkins, 'default') FROM checkins
